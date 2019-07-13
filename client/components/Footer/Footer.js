f611ecbf1ca7c11fadcfc7967346d587797e7e9d
LucyQ19 <lucyq19@gmail.com>
2019-07-13 17:16:51 -0500

    Added Footer files into Footer Component

---
+++ client/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <footer>
+      <nav className="navbar navbar-dark bg-danger">
+        <div className="container-fluid justify-content-center">
+            <p id="footerText">© LucyQ 2019</p>
+        </div>     
+      </nav>
+    </footer>
+);
+
+export default Footer;
