833200e19bed8e7d9d26a2b4a1f2f60f5ef2987d
LucyQ19 <lucyq19@gmail.com>
2019-07-13 17:19:37 -0500

    Added files to Jumbotron Component

---
+++ client/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Jumbotron.css';
+
+const Jumbotron = () => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-2">(React) Google Books Search</h1>
+            <p className="lead">Search for and save books of interest.</p>
+        </div>
+    </div>
+);
+
+export default Jumbotron;
