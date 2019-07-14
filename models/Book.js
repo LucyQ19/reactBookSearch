1ec87142570c27a9ef902d39a44decf3254989a7
LucyQ19 <lucyq19@gmail.com>
2019-07-13 22:00:26 -0500

    added content to BookResult files

--- models/Book.js
+++ models/Book.js
@@ -4,10 +4,9 @@ const Schema = mongoose.Schema;
 const BookSchema = new Schema ({
     title: { type: String, required: true},
     authors: { type: String, required: true},
-    description: String,
-    image: {type: String, trim: true},
     link: {type: String, trim: true},
-    date: { type: Date, default: Date.now }
+    img: {type: String, trim: true},
+    description: String
 });
 
 const Book = mongoose.model("Book", BookSchema);
