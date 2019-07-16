d708946ee2a224a5b66d9c64306c9763ed06240f
LucyQ19 <lucyq19@gmail.com>
2019-07-16 17:49:03 -0500

    updated API.js in utils folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 export default {
-    getBooks: () => {
-        return axios.get('/api/books');
+    getNewBooks: (title) => {
+        return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + title);
     },
 
-    searchBooks: (bookTitle) => {
-        return axios.post('/search', bookTitle)
+    getSavedBooks: () => {
+        return axios.get('/api/books')
     },
-
-    addBookToDB: (bookData) => {
-        return axios.post('/api/books', bookData);
-    }, 
 
     deleteBook: (id) => {
-        return axios.delete(`/api/books/${id}`);
+        return axios.delete('/api/books/' + id);
+    },
+
+    saveBook: (bookData) => {
+        return axios.post('/api/books', bookData)
     }
 };
